1ee974460a40ab5f36cf962c10ab4bbb68f100f4
Brandon Matran <brandon.matran@gmail.com>
2024-01-22 15:15:53 -0800

    Added Front-end pull for weather data

--- garden-gro/src/App.js
+++ garden-gro/src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from "./NavBar";
 import { Journal } from "./Journal";
 import { JournalEntry } from "./JournalEntry";
+import { WeatherAPI } from "./WeatherApi";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <NavBar />
+        <WeatherAPI />
         <Routes>
+
           <Route path="journals">
             <Route path="" element={<Journal />} />
             <Route path="post" element={<JournalEntry />} />
